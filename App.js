c3119543ecf4d55c6f5f787be6a0aae6fc2dcc7e
satish122 <satishkumar998475@gmail.com>
2021-07-07 19:13:56 +0530

    Tab Navigation one page to onother one  chapter-18

--- App.js
+++ App.js
@@ -1,36 +1,39 @@
-import React from 'react'
+
 import { View,Text,Button } from 'react-native'
 import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 
-function App(props) {
+import React, { Component } from 'react'
 
-  const data={name:'satish kumar',email:"satishkumar998475@gamil.com"}
-  return (  
-  <View> 
-    <Text style={{fontsize:20}}>App Component</Text>
-    <Button title="go to profile" onPress={()=>{props.navigation.navigate("Profile",{data})}}/>    
-  </View>
-
-  )
+ class App extends Component {
+  render() {
+    return (
+      <View> 
+      <Text style={{textAlign:"center",fontsize:30}}>Home navigation</Text>
+      </View>
+    )
+  }
 }
-const Profile=(props)=>{
-//  console.warn(props.navigation.state.param)
-  const item=props.navigation.getParam('data')
+
+function Profile()
+{
   return(
     <View>
-      <Text style={{fontSize:30}}>
-        {item.name+" "}
-        <br/>
-        {item.email+" "}
-        Profile Component1</Text>
-      <Button title="go to Home" onPress={()=>{props.navigation.navigate("Home")}}/>  
+      <Text style={{textAlign:"center",fontSize:30}}>
+        Profile navigation</Text>
+
     </View>
   )
 
 }
-const AppNavigator=createStackNavigator({
-  Home:App,
-  Profile:Profile
+const AppNavigator=createBottomTabNavigator({
+  Home:
+  {
+    screen:App
+  },
+  Profile: 
+  {
+    screen:Profile
+  }
 });
-export default createAppContainer(AppNavigator);
+export default createAppContainer(AppNavigator)

59f119f52a877796035b0442696e5d74c90b79d6
satish122 <satishkumar998475@gmail.com>
2021-07-07 16:03:43 +0530

    Stack Navigation one page to onother one with data  chapter-24

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 
 function App(props) {
+
+  const data={name:'satish kumar',email:"satishkumar998475@gamil.com"}
   return (  
   <View> 
     <Text style={{fontsize:20}}>App Component</Text>
-    <Button title="go to profile" onPress={()=>{props.navigation.navigate("Profile")}}/>    
+    <Button title="go to profile" onPress={()=>{props.navigation.navigate("Profile",{data})}}/>    
   </View>
 
   )
 }
 const Profile=(props)=>{
+  console.warn(props.navigation.getParam('data'))
+  const item=props.navigation.getParam('data')
   return(
     <View>
-      <Text style={{fontSize:30}}>Profile Component</Text>
+      <Text style={{fontSize:30}}>
+        {item.name}
+        Profile Component</Text>
       <Button title="go to Home" onPress={()=>{props.navigation.navigate("Home")}}/>  
     </View>
   )

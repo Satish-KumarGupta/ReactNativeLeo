0844885a9b59e48329fd496826067e8aa2db4041
satish122 <satishkumar998475@gmail.com>
2021-07-07 14:54:27 +0530

    Stack Navigation one page to onother one  chapter-24

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,Button } from 'react-native'
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
-import Profile from './Component/Profile'
-function App() {
+//import Profile from './Component/Profile'
+function App(props) {
   return (  
   <View> 
-    <Text style={{fontsize:20}}>Props</Text>
-    <Profile data={"Satish kumar"} />
+    <Text style={{fontsize:20}}>App Component</Text>
+    <Button title="go to profile" onPress={()=>{props.navigation.navigate("Profile")}}/>    
   </View>
 
   )
 }
-
+const Profile=()=>{
+  return(
+    <View>
+      <Text style={{fontSize:30}}>Profile Component</Text>
+    </View>
+  )
+}
 const AppNavigator=createStackNavigator({
-  Home:{
-    screen: App,
-  }
+  Home:App,
+  Profile:Profile
 });
 export default createAppContainer(AppNavigator);
